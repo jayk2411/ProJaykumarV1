351f75fcb21e59655ffcd821df56046286ca66c5
jaykumar kakadiya <jaykakdiya2411@gmail.com>
2024-08-28 21:45:29 -0400

    Multitendency to false

--- src/ProJaykumarV1.Application/Students/Dto/PagedStudentResultRequesteDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using Abp.Application.Services.Dto;
-
-namespace ProJaykumarV1.Students.Dto
-{
-    public class PagedStudentResultRequesteDto : IEntityDto<int>
-    {
-        int IEntityDto<int>.Id { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-    }
-}
--- src/ProJaykumarV1.Application/Students/StudentAppService.cs
+++ src/ProJaykumarV1.Application/Students/StudentAppService.cs
@@ -1,7 +1,7 @@
 ﻿using Abp.Application.Services;
+using Abp.Application.Services.Dto;
 using Abp.Domain.Repositories;
 using ProJaykumarV1.Models;
-using ProJaykumarV1.Students.Dto;
 
 namespace ProJaykumarV1.Students
 {
@@ -11,5 +11,25 @@ namespace ProJaykumarV1.Students
         public StudentAppService(IRepository<Student, int> repository) : base(repository)
         {
         }
+    }
+
+    public class UpdateStudentDto : IEntityDto<int>
+    {
+        int IEntityDto<int>.Id { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    }
+
+    public class CreateStudentDto : IEntityDto<int>
+    {
+        int IEntityDto<int>.Id { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    }
+
+    public class PagedStudentResultRequesteDto : IEntityDto<int>
+    {
+        int IEntityDto<int>.Id { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    }
+
+    public class StudentDto : IEntityDto<int>
+    {
+        int IEntityDto<int>.Id { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
     }
 }
